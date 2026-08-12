47d24021915d7f5c08ac6bbd03d08d2f29a15966
will4381 <91439489+will4381@users.noreply.github.com>
2026-08-12 00:06:37 -0400

    Add Snap page view pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { IM_Fell_English, Source_Serif_4 } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://willkusch.com";
@@ -71,7 +72,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${imFellEnglish.variable} ${sourceSerif.variable}`}>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script id="snap-pixel" strategy="afterInteractive">
+          {`
+            (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+            {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+            a.queue=[];var s='script';var r=t.createElement(s);r.async=!0;
+            r.src=n;var u=t.getElementsByTagName(s)[0];
+            u.parentNode.insertBefore(r,u);})(window,document,
+            'https://sc-static.net/scevent.min.js');
+
+            snaptr('init', '42d9c93c-b157-40b1-9f1e-630b11061ec3');
+            snaptr('track', 'PAGE_VIEW');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
